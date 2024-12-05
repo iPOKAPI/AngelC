696971129ec3294ad76fb2e8cac6bf9af40828ec
iPOKAPI <act.gabaril@gmail.com>
2024-12-05 22:53:32 +0800

    Upload Files

---
+++ script.js
@@ -0,0 +1,19 @@
+// Scroll Animation Logic
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+}
+
+window.addEventListener('scroll', function () {
+    const lessonContents = document.querySelectorAll('.lesson-content');
+    lessonContents.forEach(function (lessonContent) {
+        if (isInViewport(lessonContent)) {
+            lessonContent.classList.add('visible');
+        }
+    });
+});
+document.querySelector('.page-link-site').addEventListener('click', (e) => {
+    e.preventDefault(); // Ensure this line isn't blocking navigation
+  });
+
+  
