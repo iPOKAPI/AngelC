1497fa0fd6b6c4ead5e55fe61722389a2816cb3f
iPOKAPI <act.gabaril@gmail.com>
2024-12-06 02:07:10 +0800

    Upload Files

--- script.js
+++ script.js
@@ -16,4 +16,9 @@ document.querySelector('.page-link-site').addEventListener('click', (e) => {
     e.preventDefault(); // Ensure this line isn't blocking navigation
   });
 
-  
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', function() {
+    mobileMenu.classList.toggle('show');
+});
